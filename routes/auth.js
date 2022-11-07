423d9b349a08d3abca43a4d20ef935529cd2c658
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2022-11-07 10:59:26 +0600

    added Express API rate Limit

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const authControllers = require("../controllers/auth");
+const rateLimiter = require("../helpers/rateLimiter");
 
 // Router initialisation
 const router = express.Router();
 
 // Routes
-router.get("/test", authControllers.test);
+router.get("/test", rateLimiter(1, 10), authControllers.test);
 
 module.exports = router;
